ec6a1476e604895f2244e8dbb7b149e7ebe826f7
Aliece <alieceyy@gmail.com>
2015-08-27 20:01:01 +0800

    all

--- src/main/java/com/jdon/rpc/proxy/DoCGLib.java
+++ src/main/java/com/jdon/rpc/proxy/DoCGLib.java
@@ -11,7 +11,6 @@ public class DoCGLib {
     public static void main(String[] args) {
         Constructor[] sss = Hello.class.getDeclaredConstructors();
 
-
         CglibProxy proxy = new CglibProxy();
         //通过生成子类的方式创建代理类
         Hello sproxy = (Hello) proxy.getProxy(Hello.class);
